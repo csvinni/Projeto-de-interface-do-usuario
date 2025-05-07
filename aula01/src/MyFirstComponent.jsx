67b4a84da33695cedfb59916ee1a22e795eb7835
Vinicios <dantas.vinicios@escolar.ifrn.edu.br>
2025-05-07 17:35:24 -0300

    aula1.1

---
+++ aula01/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function MyFirstComponent (){
+    const nome = "vinicios"
+
+    return(
+        <>
+        <div>
+            <h1>Hello Word!!!</h1>
+        </div>
+        <div>
+            <p>Seu nome é {nome}</p>
+            <p>a soma de 2+2 é {2+2}</p>
+        </div>
+        </>
+    )
+}
+
+export default MyFirstComponent
